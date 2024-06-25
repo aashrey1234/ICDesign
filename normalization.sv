f8f91288c3c978ba0f782b017143210a70b09099
aashrey1234 <155153682+aashrey1234@users.noreply.github.com>
2024-06-25 10:48:07 +0530

    Update normalization.sv

--- normalization.sv
+++ normalization.sv
@@ -19,41 +19,6 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-//module image_pixel_normalizer
-//#(
-//    parameter DATA_WIDTH = 8, 
-//    parameter IMAGE_WIDTH = 4, 
-//    parameter IMAGE_HEIGHT = 4 
-//)
-//(
-//    input logic clk,
-//    input logic reset,
-//    input logic [DATA_WIDTH-1:0] min_val, 
-//    input logic [DATA_WIDTH-1:0] max_val, 
-//    input logic [DATA_WIDTH-1:0] data_in[IMAGE_WIDTH][IMAGE_HEIGHT], 
-//    output logic [DATA_WIDTH-1:0] data_out[IMAGE_WIDTH][IMAGE_HEIGHT] 
-//);
-
-    
-//    always_ff @(posedge clk or posedge reset) begin
-//        if (reset) begin
-//            foreach(data_out[i,j])
-//            data_out[i][j] <= 0;
-//        end
-//        else 
-//        for (int i = 0; i < IMAGE_WIDTH ; i++) begin
-//            for (int j = 0; j < IMAGE_HEIGHT; j++) begin
-//                if (max_val != min_val) begin
-//                    data_out[i][j] <= (data_in[i][j] - min_val) * ((1 << DATA_WIDTH) - 1) / (max_val - min_val);
-//                    end  
-//                else begin
-//                    data_out[i][j] <= 0; 
-//                    end
-//            end
-//           end
-//          end
-//endmodule
-
 module matrix_pixel_normalizer
 #(
     parameter int DATA_WIDTH = 8, 
